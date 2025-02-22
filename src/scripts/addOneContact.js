0799fb23bff3cfd3cea9c372cc261e8e38110693
Andrii Ishchuk <a_ishchuk22@ukr.net>
2025-02-22 12:45:14 +0300

    addOne done

--- src/scripts/addOneContact.js
+++ src/scripts/addOneContact.js
@@ -1,3 +1,17 @@
-export const addOneContact = async () => {};
+import { createFakeContact } from '../utils/createFakeContact.js';
+import { readContacts } from '../utils/readContacts.js';
+import { PATH_DB } from '../constants/contacts.js';
+import * as fs from 'node:fs/promises';
+
+export const addOneContact = async () => {
+  try {
+    const existingContacts = await readContacts();
+    const newContact = [...existingContacts, createFakeContact()];
+    await fs.writeFile(PATH_DB, JSON.stringify(newContact, null, 2));
+    console.log('Контакт додано!');
+  } catch (err) {
+    console.error(err);
+  }
+};
 
 addOneContact();
